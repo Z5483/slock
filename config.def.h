d06c94bcb8c865e54f98132ec319b5cf522f862c
Khue Nguyen <Z5483Y@gmail.com>
2020-08-19 02:51:33 -0400

    Modify default configuration

--- config.def.h
+++ config.def.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#181818",     /* after initialization */
+	[INPUT] =  "#181818",   /* during input */
+	[FAILED] = "#ab4642",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
